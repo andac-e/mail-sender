18949b974ab4deea28f233129147731e18a69253
andac-e <65905654+andac-e@users.noreply.github.com>
2021-05-11 13:59:58 +0300

    Add project files.

---
+++ MVC_MailService_Tekrar/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MVC_MailService_Tekrar.DesignPatterns.SingletonPattern;
+using MVC_MailService_Tekrar.Models.Context;
+using MVC_MailService_Tekrar.Models.Entities;
+using MVC_MailService_Tekrar.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_MailService_Tekrar.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        MyContext _db;
+
+        public HomeController()
+        {
+            _db = DBTool.DBInstance;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(AppUser appUser)
+        {
+            MailService.Send(appUser.Email,body:"Hello World:)",subject:":)");
+            ViewBag.Message = "Mail başarılı bir şekilde gönderilmiştir";
+            return View();
+        }
+    }
+}
